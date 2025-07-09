72b40881e5494fe15561c01b427d1f8e347100a3
Yash Maharjan <yash.maharjan@yipl.com.np>
2025-07-10 00:05:51 +0545

    -[x] Deploy Script Addition

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/organisms/Container";
 import Hero from "@/components/organisms/Hero";
 import Navbar from "@/components/organisms/Navbar";
+import CheapFlights from "@/components/sections/CheapFlights";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Container>
         <Hero />
+        <CheapFlights />
       </Container>
     </div>
   );
