f4591ec579b68c14d5ceee8a6137c5f998be46a9
Yash Maharjan <yash.maharjan@yipl.com.np>
2025-07-10 23:04:19 +0545

    -[x] Border Alternating Select City

--- components/sections/FlightSchedule/index.tsx
+++ components/sections/FlightSchedule/index.tsx
@@ -14,11 +14,12 @@ const FlightSchedule = () => {
   return (
     <div className="mt-5 grid grid-cols-5 gap-5">
       <div className="flex items-center gap-2 col-span-5 md:col-span-3 relative ">
-        <div className="absolute size-7 border bg-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-3 rounded-full">
-          <LucideArrowRightLeft className="size-4 -translate-x-[40%] -translate-y-[40%]" />
+        <div className="absolute size-8 z-10 bg-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-3 rounded-full">
+          <LucideArrowRightLeft className="size-4 -translate-x-[30%] -translate-y-[30%] text-gray-500" />
         </div>
         <Select>
-          <SelectTrigger className="py-6 focus:outline-0 focus:ring-0">
+          <SelectTrigger className="py-6 focus:outline-0 relative group focus:ring-0 hover:border-gray-400 overflow-hidden">
+            <div className="size-8 rounded-full absolute border group-hover:border-gray-400 -right-5"></div>
             <SelectIcon asChild>
               <CircleDot />
             </SelectIcon>
@@ -36,7 +37,8 @@ const FlightSchedule = () => {
         </Select>
 
         <Select>
-          <SelectTrigger className=" py-6 focus:outline-0 focus:ring-0">
+          <SelectTrigger className=" py-6 focus:outline-0 focus:ring-0 group hover:border-gray-400 overflow-hidden relative">
+            <div className="size-8 rounded-full absolute border group-hover:border-gray-400 -left-5"></div>
             <SelectIcon asChild>
               <MapPin />
             </SelectIcon>
